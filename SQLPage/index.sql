18ce597fa74ebac2f17a81daf5f12b3cd1c7a329
Karolinova <karolina.nowinska13@gmail.com>
2024-12-17 20:36:00 +0100

    add labels and rename validate and title

--- SQLPage/index.sql
+++ SQLPage/index.sql
@@ -1,17 +1,18 @@
 select 'dynamic' as component, sqlpage.run_sql('shell.sql') as properties;
 
 select 'form' as component
-, 'Logowanie' as validate
+, 'Zaloguj się' as validate
 , 'Link_weryfikacja.sql' as action -- page to verify if user exists in base
-, 'Zaloguj się' as title
+, 'Logowanie' as title
 ; 
 
-select 'Login' as name
+select 'login' as name
+, 'Login' as label
 , 'text' as type
 , true as required
 ;
 
-select 'Haslo' as name
+select 'password' as name
 , 'Hasło' as label -- to display name
 , 'password' as type -- to hide password on display
 , true as required
