c36593e4c298274a50335f0d66d8b323cc853480
Karolinova <karolina.nowinska13@gmail.com>
2024-12-17 20:47:22 +0100

    add welcome to main page

--- SQLPage/Strona_glowna.sql
+++ SQLPage/Strona_glowna.sql
@@ -1,4 +1,9 @@
 select 'dynamic' as component, sqlpage.run_sql('shell.sql') as properties;
 
 select 'text' as component
-    , 'Witamy' as contents;
+    , 'Witamy ' || 
+    (select u.login 
+    from users u 
+    join user_log ul on ul.user_id=u.id
+    where ul.token=sqlpage.cookie('session_token')) 
+    as contents;
