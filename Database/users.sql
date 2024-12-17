c19021c08382eb0b1833973c233fe4ea1c111380
Karolinova <karolina.nowinska13@gmail.com>
2024-12-17 19:39:27 +0100

    add column and change data types

--- Database/users.sql
+++ Database/users.sql
@@ -1,6 +1,7 @@
 -- Table users to store data of users
-create table users (login varchar(20)
+create table users (id bigint primary key
+  , login varchar(20)
   , password varchar(20)
-  , read_only bit
+  , read_only boolean
   , password_hash varchar(100)
   );
