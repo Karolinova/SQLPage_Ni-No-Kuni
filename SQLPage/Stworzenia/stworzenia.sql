69a16e2308a4637194a886f20713b0cd3259f1a8
Karolinova <karolina.nowinska13@gmail.com>
2025-01-04 22:29:18 +0100

    add the beginning of the first step of filtration

--- SQLPage/Stworzenia/stworzenia.sql
+++ SQLPage/Stworzenia/stworzenia.sql
@@ -1,2 +1,12 @@
 select 'dynamic' as component
 , sqlpage.run_sql('shell.sql') AS properties;
+
+-- Add filtration
+select 'form' as component
+, 'Filtrowanie po stworzeniach' as title
+, 'Filtruj' as validate
+, 'stworzenia_filtrowanie.sql' as action;
+
+select 'Rodzaj' as name
+,  'input' as type
+;
