3a42d8b17b8d4c42ee141474054e7e92284345b2
Karolinova <karolina.nowinska13@gmail.com>
2025-01-11 14:39:36 +0100

    add extra information

--- Database/Ni_No_Kuni/triki_zaklecia.sql
+++ Database/Ni_No_Kuni/triki_zaklecia.sql
@@ -1,4 +1,4 @@
--- table 'triki_zaklecia'
+-- table 'triki_zaklecia' (tricks and spells of familiars and characters)
 create table triki_zaklecia (id bigserial primary key
 , nazwa varchar
 , stworzenie_postac boolean
