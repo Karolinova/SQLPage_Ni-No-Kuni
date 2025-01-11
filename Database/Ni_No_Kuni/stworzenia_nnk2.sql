5b4507021eac37a9b42cde66fabb43cd6f4ff996
Karolinova <karolina.nowinska13@gmail.com>
2025-01-11 14:39:01 +0100

    add extra information

--- Database/Ni_No_Kuni/stworzenia_nnk2.sql
+++ Database/Ni_No_Kuni/stworzenia_nnk2.sql
@@ -1,4 +1,4 @@
--- table 'stworzenia_nnk2'
+-- table 'stworzenia_nnk2' (familiars from Ni No Kuni II: Revenant Kingdom)
 create table stworzenia_nnk2 (stw_id bigserial primary key
 , imie varchar
 , constraint FK_stworzenia2 foreign key(stw_id) references stworzenia (id)
