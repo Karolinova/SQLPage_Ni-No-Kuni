7dc1e4a6910dd679259b30cefe15c7ff6b6d2427
Karolinova <karolina.nowinska13@gmail.com>
2025-01-11 14:38:14 +0100

    add extra information

--- Database/Ni_No_Kuni/stworzenia_nnk1.sql
+++ Database/Ni_No_Kuni/stworzenia_nnk1.sql
@@ -1,4 +1,4 @@
--- table 'stworzenia_nnk1'
+-- table 'stworzenia_nnk1' (familiars from Ni No Kuni: Wrath of The White Witch)
 create table stworzenia_nnk1 (stw_id bigserial primary key
 , imie varchar
 , constraint FK_stworzenia1 foreign key(stw_id) references stworzenia (id)
