a8650d800d397af8d34bde833d3a1cfb2c87ea0e
Karolinova <karolina.nowinska13@gmail.com>
2025-01-11 14:34:54 +0100

    create table stworzenia

---
+++ Database/Ni_No_Kuni/stworzenia.sql
@@ -0,0 +1,12 @@
+-- Table 'Stworzenia'
+create table stworzenia (id bigserial primary key
+, nazwa varchar
+, gatunek varchar
+, przysmak_id int
+, trik1_id int
+, trik2_id int
+, trik3_id int
+, constraint FK_triki_zaklecia1 foreign key(trik1_id) references triki_zaklecia (id)
+, constraint FK_triki_zaklecia2 foreign key(trik2_id) references triki_zaklecia (id)
+, constraint FK_triki_zaklecia3 foreign key(trik3_id) references triki_zaklecia (id)
+);
