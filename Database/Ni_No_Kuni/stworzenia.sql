4689690f4fbe17c537bea6ae8c0c0a61dab96c8a
Karolinova <karolina.nowinska13@gmail.com>
2025-01-17 21:14:33 +0100

    add constraint to the table

--- Database/Ni_No_Kuni/stworzenia.sql
+++ Database/Ni_No_Kuni/stworzenia.sql
@@ -10,3 +10,7 @@ create table stworzenia (id bigserial primary key
 , constraint FK_triki_zaklecia2 foreign key(trik2_id) references triki_zaklecia (id)
 , constraint FK_triki_zaklecia3 foreign key(trik3_id) references triki_zaklecia (id)
 );
+
+-- add connection between two tables: stworzenia and recepta
+alter table stworzenia 
+add constraint FK_recepta foreign key(przysmak_id) references recepta (id);
