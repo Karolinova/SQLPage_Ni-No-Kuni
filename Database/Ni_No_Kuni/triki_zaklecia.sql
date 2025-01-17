72e8da68b8700745f852867775dd96c00098ef1d
Karolinova <karolina.nowinska13@gmail.com>
2025-01-17 21:27:11 +0100

    modyfiy column type

--- Database/Ni_No_Kuni/triki_zaklecia.sql
+++ Database/Ni_No_Kuni/triki_zaklecia.sql
@@ -3,3 +3,6 @@ create table triki_zaklecia (id bigserial primary key
 , nazwa varchar
 , stworzenie_postac boolean
 );
+
+-- modyfiy column type - possible values S (stworzenia - familiars) or P (postacie - characters)
+ALTER TABLE triki_zaklecia ALTER COLUMN stworzenie_postac type varchar(10);
