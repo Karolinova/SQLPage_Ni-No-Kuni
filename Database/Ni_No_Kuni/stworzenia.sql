bccfce3f3df0e962d1a2a7e63d4ed063219c3255
Karolinova <karolina.nowinska13@gmail.com>
2025-01-31 21:48:33 +0100

    changes after creating a table 'gatunek'

--- Database/Ni_No_Kuni/stworzenia.sql
+++ Database/Ni_No_Kuni/stworzenia.sql
@@ -14,3 +14,16 @@ create table stworzenia (id bigserial primary key
 -- add connection between two tables: stworzenia and recepta
 alter table stworzenia 
 add constraint FK_recepta foreign key(przysmak_id) references recepta (id);
+
+-- change column 'gatunek'
+alter table stworzenia
+add column gatunek_id int references gatunek(id);
+
+update stworzenia s
+set gatunek_id = g.id
+from gatunek g
+where s.gatunek = g.nazwa;
+
+alter table stworzenia drop column gatunek;
+
+alter table stworzenia drop column przysmak_id;
