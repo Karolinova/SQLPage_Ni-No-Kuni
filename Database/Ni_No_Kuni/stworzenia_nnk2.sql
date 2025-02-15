dd61b063f4cafa4a78b9cd3f326f0c65dc9af7c2
Karolinova <karolina.nowinska13@gmail.com>
2025-02-15 14:02:24 +0100

    drop table stworzenia_nnk2

--- Database/Ni_No_Kuni/stworzenia_nnk2.sql
+++
@@ -1,5 +0,0 @@
--- table 'stworzenia_nnk2' (familiars from Ni No Kuni II: Revenant Kingdom)
-create table stworzenia_nnk2 (stw_id bigserial primary key
-, imie varchar
-, constraint FK_stworzenia2 foreign key(stw_id) references stworzenia (id)
-);
