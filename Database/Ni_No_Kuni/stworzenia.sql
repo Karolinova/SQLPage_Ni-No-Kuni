94dc29adab9ad3f2f1c49b3458dbbc8efa01ba9e
Karolinova <karolina.nowinska13@gmail.com>
2025-02-15 13:59:11 +0100

    modify table stworzenia

--- Database/Ni_No_Kuni/stworzenia.sql
+++ Database/Ni_No_Kuni/stworzenia.sql
@@ -1,29 +1,13 @@
 -- Table 'Stworzenia' (all familiars)
 create table stworzenia (id bigserial primary key
 , nazwa varchar
-, gatunek varchar
-, przysmak_id int
 , trik1_id int
 , trik2_id int
 , trik3_id int
 , constraint FK_triki_zaklecia1 foreign key(trik1_id) references triki_zaklecia (id)
 , constraint FK_triki_zaklecia2 foreign key(trik2_id) references triki_zaklecia (id)
 , constraint FK_triki_zaklecia3 foreign key(trik3_id) references triki_zaklecia (id)
+, gatunek_id int
+, constraint FK_gatunek foreign key(gatunek_id) references gatunek (g_id)
 );
 
--- add connection between two tables: stworzenia and recepta
-alter table stworzenia 
-add constraint FK_recepta foreign key(przysmak_id) references recepta (id);
-
--- change column 'gatunek'
-alter table stworzenia
-add column gatunek_id int references gatunek(id);
-
-update stworzenia s
-set gatunek_id = g.id
-from gatunek g
-where s.gatunek = g.nazwa;
-
-alter table stworzenia drop column gatunek;
-
-alter table stworzenia drop column przysmak_id;
