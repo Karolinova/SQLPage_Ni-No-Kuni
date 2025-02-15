92b6af839ef37a64c3fd16546bcf1c96a5f5fa54
Karolinova <karolina.nowinska13@gmail.com>
2025-02-15 14:03:02 +0100

    modify table triki_zaklecia

--- Database/Ni_No_Kuni/triki_zaklecia.sql
+++ Database/Ni_No_Kuni/triki_zaklecia.sql
@@ -1,8 +1,6 @@
 -- table 'triki_zaklecia' (tricks and spells of familiars and characters)
 create table triki_zaklecia (id bigserial primary key
 , nazwa varchar
-, stworzenie_postac boolean
+, stworzenie_postac varchar(10)
 );
 
--- modyfiy column type - possible values S (stworzenia - familiars) or P (postacie - characters)
-ALTER TABLE triki_zaklecia ALTER COLUMN stworzenie_postac type varchar(10);
