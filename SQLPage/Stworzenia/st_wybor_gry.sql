f17444ecec654276b9bfcbc8b5d4661a484a33fb
Karolinova <karolina.nowinska13@gmail.com>
2025-03-14 18:46:29 +0100

    add more column and modify filtering

--- SQLPage/Stworzenia/st_wybor_gry.sql
+++ SQLPage/Stworzenia/st_wybor_gry.sql
@@ -96,7 +96,9 @@ with imie as (
         'value', nazwa
     )) as OPTIONS
     , 2 as width
-    from  triki_zaklecia
+    from (
+        select nazwa, stworzenie_postac from triki_zaklecia ORDER BY nazwa
+        ) triki_zaklecia
     where stworzenie_postac = 'S'
 )
 select * from imie
@@ -113,16 +115,20 @@ select * from triki
 -- Add filter result of the selected game
 select 'table' as component;
 
-select imie, nazwa, gatunek, nazwa_rec as "Przysmak", "Trik 1", "Trik 2", "Trik 3"
+select imie, nazwa, gatunek, nazwa_rec as "Przysmak", "Trik 1", "Trik 2", "Trik 3", "Trik 4", "Trik 5", "Trik 6"
       from (
          select 'Ni no Kuni: Wrath of the White Witch' as gra
          , stw.imie, s.nazwa, g.nazwa as gatunek, r.nazwa as nazwa_rec
          , t1.nazwa as "Trik 1", t2.nazwa as "Trik 2", t3.nazwa as "Trik 3"
+         , t4.nazwa as "Trik 4", t5.nazwa as "Trik 5", t6.nazwa as "Trik 6"
          from stworzenia s 
          join stworzenia_nnk stw on stw.stw_id = s.id
          join triki_zaklecia t1 on t1.id = s.trik1_id
          join triki_zaklecia t2 on t2.id = s.trik2_id
          left join triki_zaklecia t3 on t3.id = s.trik3_id
+         left join triki_zaklecia t4 on t4.id = s.trik4_id
+         left join triki_zaklecia t5 on t5.id = s.trik5_id
+         left join triki_zaklecia t6 on t6.id = s.trik6_id
          join gatunek g on g.g_id = s.gatunek_id
          join recepta r on r.id = g.przysmak_id
          where stw.gra_id = 1
@@ -130,11 +136,15 @@ select imie, nazwa, gatunek, nazwa_rec as "Przysmak", "Trik 1", "Trik 2", "Trik 
          select 'Ni no Kuni II: Revenant Kingdom' as gra
          , stw.imie, s.nazwa, g.nazwa as gatunek, r.nazwa as nazwa_rec
          , t1.nazwa as "Trik 1", t2.nazwa as "Trik 2", t3.nazwa as "Trik 3"
+         , t4.nazwa as "Trik 4", t5.nazwa as "Trik 5", t6.nazwa as "Trik 6"
          from stworzenia s 
          join stworzenia_nnk stw on stw.stw_id = s.id
          join triki_zaklecia t1 on t1.id = s.trik1_id
          join triki_zaklecia t2 on t2.id = s.trik2_id
          left join triki_zaklecia t3 on t3.id = s.trik3_id
+         left join triki_zaklecia t4 on t4.id = s.trik4_id
+         left join triki_zaklecia t5 on t5.id = s.trik5_id
+         left join triki_zaklecia t6 on t6.id = s.trik6_id
          join gatunek g on g.g_id = s.gatunek_id
          join recepta r on r.id = g.przysmak_id
          where stw.gra_id = 2
@@ -142,25 +152,34 @@ select imie, nazwa, gatunek, nazwa_rec as "Przysmak", "Trik 1", "Trik 2", "Trik 
          select 'Ni No Kuni I/Ni no Kuni II' as gra
          , nnk1_nnk2.imie, nnk1_nnk2.nazwa, nnk1_nnk2.gatunek, nnk1_nnk2.nazwa_rec
          , nnk1_nnk2."Trik 1", nnk1_nnk2."Trik 2", nnk1_nnk2."Trik 3"
+         , nnk1_nnk2."Trik 4", nnk1_nnk2."Trik 5", nnk1_nnk2."Trik 6"
          from
          (select stw.imie, s.nazwa, g.nazwa as gatunek, r.nazwa as nazwa_rec
          , t1.nazwa as "Trik 1", t2.nazwa as "Trik 2", t3.nazwa as "Trik 3"
+         , t4.nazwa as "Trik 4", t5.nazwa as "Trik 5", t6.nazwa as "Trik 6"
          from stworzenia s 
          join stworzenia_nnk stw on stw.stw_id = s.id
          join triki_zaklecia t1 on t1.id = s.trik1_id
          join triki_zaklecia t2 on t2.id = s.trik2_id
          left join triki_zaklecia t3 on t3.id = s.trik3_id
+         left join triki_zaklecia t4 on t4.id = s.trik4_id
+         left join triki_zaklecia t5 on t5.id = s.trik5_id
+         left join triki_zaklecia t6 on t6.id = s.trik6_id
          join gatunek g on g.g_id = s.gatunek_id
          join recepta r on r.id = g.przysmak_id
          where stw.gra_id = 1
          union all
          select stw.imie, s.nazwa, g.nazwa as gatunek, r.nazwa as nazwa_rec
          , t1.nazwa as "Trik 1", t2.nazwa as "Trik 2", t3.nazwa as "Trik 3"
+         , t4.nazwa as "Trik 4", t5.nazwa as "Trik 5", t6.nazwa as "Trik 6"
          from stworzenia s 
          join stworzenia_nnk stw on stw.stw_id = s.id
          join triki_zaklecia t1 on t1.id = s.trik1_id
          join triki_zaklecia t2 on t2.id = s.trik2_id
          left join triki_zaklecia t3 on t3.id = s.trik3_id
+         left join triki_zaklecia t4 on t4.id = s.trik4_id
+         left join triki_zaklecia t5 on t5.id = s.trik5_id
+         left join triki_zaklecia t6 on t6.id = s.trik6_id
          join gatunek g on g.g_id = s.gatunek_id
          join recepta r on r.id = g.przysmak_id
          where stw.gra_id = 2) nnk1_nnk2
