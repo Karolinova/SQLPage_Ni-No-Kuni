2f50ef8abc0aa47768e1828f1c905d1b4527ea38
Karolinova <karolina.nowinska13@gmail.com>
2025-05-31 21:16:58 +0200

    page and form to add familiar

---
+++ SQLPage/Stworzenia/Akcje/dodaj_stworzenie.sql
@@ -0,0 +1,40 @@
+select 'dynamic' as component
+, sqlpage.run_sql('shell.sql') AS properties;
+
+-- Add return button
+ Select 'button' as component
+    ,'sm' as size;
+select 
+    '/Stworzenia/st_wybor_gry.sql?id='||id||'' as link,
+    'Powrót'  as title
+    from help_list
+    where nazwa='Gra'
+    and id = $gra::BIGINT;
+
+-- Add form to add data
+select 'form' as component
+, 'Dodaj nowe stworzenie' as title
+, 'Dodaj' as validate
+, 'ins_stw.sql?gra='||$gra||'' as action 
+;
+
+-- Name
+select 'Imię' as label
+, 'imie' as name
+, true as required
+;
+
+-- Name of genus
+select 'select' as type
+, 'nazwa' as name
+, 'Nazwa' as label
+, true as required
+, 'pytanie' as class
+, '[{"label": "Wybierz nazwę", "value": "Wybierz nazwę"}]'::jsonb || jsonb_agg(json_build_object(
+        'label', nazwa,
+        'value', nazwa
+    )) as OPTIONS
+    from (
+        select nazwa from stworzenia order by nazwa
+        ) nazwa
+;
