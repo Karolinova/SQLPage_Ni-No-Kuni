668162da892aa92f907c9dfa51acb3cbbe862be3
Karolinova <karolina.nowinska13@gmail.com>
2025-06-08 10:30:02 +0200

    trigger to update correctly gold column

---
+++ Database/Ni_No_Kuni/trigger_update_gold.sql
@@ -0,0 +1,14 @@
+create or replace function update_stw_gold ()
+returns trigger as $$
+begin 
+	update stworzenia
+	set gold = case when nazwa like '%(gold)' then true else false end
+	where id = new.id;
+	return null;
+end;
+$$ LANGUAGE plpgsql;
+
+create TRIGGER update_gold
+after insert on stworzenia
+for each row
+execute function update_stw_gold();
