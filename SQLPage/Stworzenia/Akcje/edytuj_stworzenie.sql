d34dc675dbe6052492d47461f5e85649b701db6c
Karolinova <karolina.nowinska13@gmail.com>
2025-06-15 12:21:22 +0200

    change return button after add pagination

--- SQLPage/Stworzenia/Akcje/edytuj_stworzenie.sql
+++ SQLPage/Stworzenia/Akcje/edytuj_stworzenie.sql
@@ -5,7 +5,7 @@ select 'dynamic' as component
  Select 'button' as component
     ,'sm' as size;
 select 
-    '/Stworzenia/st_wybor_gry.sql?id='||id||'' as link,
+    '/Stworzenia/st_wybor_gry.sql?id='||id||'&offset=0&page=10' as link,
     'Powrót'  as title
     from help_list
     where nazwa='Gra'
