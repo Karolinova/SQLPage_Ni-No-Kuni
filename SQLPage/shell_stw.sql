f870def1aee7b3d16dfb24e981712dc569b0d640
Karolinova <karolina.nowinska13@gmail.com>
2025-08-31 19:06:27 +0200

    change visuality by shell

---
+++ SQLPage/shell_stw.sql
@@ -0,0 +1,9 @@
+select 'shell' as component
+-- , 'boxed' as layout
+, 'Twórca: Karolinova' as footer
+, '/style.css' as css
+, true as sidebar
+, jsonb_build_array(
+  jsonb_build_object('title', 'Stworzenia', 'link', '/Stworzenia/stworzenia_user.sql?gra=' ||$gra||'')
+) as menu_item
+;
