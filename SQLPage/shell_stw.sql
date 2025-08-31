fba6539baf5d10e4ee31422a1223c837efe2742e
Karolinova <karolina.nowinska13@gmail.com>
2025-08-31 19:22:35 +0200

    add link with admin page - data to new familiar

--- SQLPage/shell_stw.sql
+++ SQLPage/shell_stw.sql
@@ -6,4 +6,11 @@ select 'shell' as component
 , jsonb_build_array(
   jsonb_build_object('title', 'Stworzenia', 'link', '/Stworzenia/stworzenia_user.sql?gra=' ||$gra||'')
 ) as menu_item
+, (select jsonb_build_array(
+  jsonb_build_object('title', 'Nowe stworzenie', 'link', '/Admin/nowe_stworzenie.sql?gra=' ||$gra||'')
+) as menu_item
+from users
+where id = (select user_id from user_log
+            where token=sqlpage.cookie('session_token'))
+  and login = 'Admin') as menu_item
 ;
