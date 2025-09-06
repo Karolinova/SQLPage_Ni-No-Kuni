9f1181f9a7d37ed29e22a7d16973ebfafec32567
Karolinova <karolina.nowinska13@gmail.com>
2025-09-06 10:50:57 +0200

    modify shell componen - add new menu_item and change name of another

--- SQLPage/shell_stw.sql
+++ SQLPage/shell_stw.sql
@@ -7,7 +7,14 @@ select 'shell' as component
   jsonb_build_object('title', 'Stworzenia', 'link', '/Stworzenia/stworzenia_user.sql?gra=' ||$gra||'')
 ) as menu_item
 , (select jsonb_build_array(
-  jsonb_build_object('title', 'Nowe stworzenie', 'link', '/Admin/nowe_stworzenie.sql?gra=' ||$gra||'')
+  jsonb_build_object('title', 'Dodaj nowe stworzenie', 'link', '/Admin/nowe_stworzenie.sql?gra=' ||$gra||'')
+) as menu_item
+from users
+where id = (select user_id from user_log
+            where token=sqlpage.cookie('session_token'))
+  and login = 'Admin') as menu_item
+, (select jsonb_build_array(
+  jsonb_build_object('title', 'Modyfikuj / usuń stworzenie', 'link', '/Admin/modyfikuj_usun_stworzenie.sql?gra=' ||$gra||'')
 ) as menu_item
 from users
 where id = (select user_id from user_log
