fca7da12f549d6abc11c4a5f1e0d493657cfd97f
Karolinova <karolina.nowinska13@gmail.com>
2025-09-06 10:26:13 +0200

    correct function and triggers

--- Database/Ni_No_Kuni/trigger_update_gold.sql
+++ Database/Ni_No_Kuni/trigger_update_gold.sql
@@ -1,19 +1,17 @@
 create or replace function update_stw_gold ()
 returns trigger as $$
 begin 
-	update stworzenia
-	set gold = case when nazwa like '%(gold)' then true else false end
-	where id = new.id;
-	return null;
+	NEW.gold := (NEW.nazwa like '%(gold)');
+    return NEW;
 end;
 $$ LANGUAGE plpgsql;
 
 create TRIGGER update_gold
-after insert on stworzenia
+before insert on stworzenia
 for each row
 execute function update_stw_gold();
 
 create TRIGGER ins_update_gold
-after update on stworzenia
+before update on stworzenia
 for each row
 execute function update_stw_gold();
