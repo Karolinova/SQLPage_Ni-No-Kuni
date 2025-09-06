35ec1a6ea0569d44692810cbcfcb4c800f8ca395
Karolinova <karolina.nowinska13@gmail.com>
2025-09-06 10:48:17 +0200

    create page to edit/delete familiar

---
+++ SQLPage/Admin/Akcje/modyfikuj_stw.sql
@@ -0,0 +1,70 @@
+select 'dynamic' as component
+, sqlpage.run_sql('shell_stw.sql') AS properties;
+
+-- Add return button
+ Select 'button' as component
+    ,'sm' as size;
+select 
+    '/Admin/modyfikuj_usun_stworzenie.sql?gra='||$gra||'' as link,
+    'Powrót'  as title;
+
+-- Add header
+select 'title' as component
+, 'Modyfikuj dane stworzenia' as contents
+, 3 as LEVEL
+, true as center
+;
+
+-- Add form to add data
+select 'form' as component
+, 'Dane stworzenia' as title
+, 'Modyfikuj' as validate
+;
+
+-- Name of genus
+select 'Nazwa' as label
+, 'name' as name
+, true as required
+, nazwa as value
+from stworzenia 
+where id = $id::bigint
+;
+
+-- Genus
+select 'select' as type
+, 'gatunek' as name
+, 'Gatunek' as label
+, jsonb_agg(json_build_object(
+    'label', nazwa,
+    'value', g_id,
+    'selected', nazwa = (select g.nazwa 
+        from stworzenia s 
+        join gatunek g on g.g_id = s.gatunek_id
+        where s.id = $id::BIGINT)
+)) as OPTIONS
+from (select distinct g.nazwa, g_id
+        from gatunek g order by g.nazwa ) g
+;
+
+with selected as (
+  select
+    array_remove(array[
+      s.trik1_id, s.trik2_id, s.trik3_id,
+      s.trik4_id, s.trik5_id, s.trik6_id
+    ], NULL)::int[] as ids
+  from stworzenia s
+  where s.id = $id::bigint
+)
+-- Tricks
+select 'select' as type
+, 'trik[]' as name
+, 'Trik' as label
+, true as multiple
+, jsonb_agg(json_build_object(
+    'label', nazwa,
+    'value', id,
+    'selected', id = any(coalesce((select ids from selected limit 1), ARRAY[]::int[]))
+)) as OPTIONS
+from (select g.nazwa, g.id
+        from triki_zaklecia g 
+        where g.stworzenie_postac = 'S' order by g.nazwa ) g;
---
+++ SQLPage/Admin/modyfikuj_usun_stworzenie.sql
@@ -0,0 +1,77 @@
+select 'dynamic' as component
+, sqlpage.run_sql('shell_stw.sql') AS properties;
+
+-- Add return button
+ Select 'button' as component
+    ,'sm' as size;
+select 
+    '/strona_glowna_gry.sql?gra='||$gra||'' as link,
+    'Powrót'  as title;
+
+select 
+    'title'   as component
+    , 'Lista stworzeń' as contents
+    , true as center;
+
+select 
+    'tab' as component,
+    TRUE  as center;
+select 'WSZYSTKO' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1' as link;
+select 'A' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=A' as link;
+select 'B' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=B' as link;
+select 'C' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=C' as link;
+select 'D' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=D' as link;
+select 'E' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=E' as link;
+select 'F' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=F' as link;
+select 'G' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=G' as link;
+select 'H' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=H' as link;
+select 'I' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=I' as link;
+select 'J' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=J' as link;
+select 'K' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=K' as link;
+select 'L' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=L' as link;
+select 'M' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=M' as link;
+select 'N' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=N' as link;
+select 'O' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=O' as link;
+select 'P' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=P' as link;
+select 'R' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=R' as link;
+select 'S' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=S' as link;
+select 'T' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=T' as link;
+select 'U' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=U' as link;
+select 'W' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=W' as link;
+select 'Z' as title
+, 'modyfikuj_usun_stworzenie.sql?gra=1&litera=Z' as link;
+
+
+select 'list' as component
+, TRUE as search
+;
+
+select nazwa as title
+, 'Akcje/modyfikuj_stw.sql?gra='||$gra||'&id='||id||'' as edit_link
+, '[Edytuj](Akcje/usun_stw.sql?gra='||$gra||'&id='||id||')' as delete_link
+from stworzenia
+where gra_id = $gra::int
+and (left(nazwa,1) ilike $litera or $litera is null)
+order by nazwa;
