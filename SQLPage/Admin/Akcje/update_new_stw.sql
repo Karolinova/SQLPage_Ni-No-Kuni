e57f8f14b22a414f2a543b08a0cc55650c693e3b
Karolinova <karolina.nowinska13@gmail.com>
2025-09-18 21:33:35 +0200

    update and then direct

---
+++ SQLPage/Admin/Akcje/update_new_stw.sql
@@ -0,0 +1,26 @@
+with licznik_trikow as (
+    select jsonb_array_length(:trik::jsonb) as i
+)
+, aktualizuj_dane as (
+    update stworzenia
+    set nazwa = $name
+    , gatunek_id = $gatunek::BIGINT
+    , trik1_id = (:trik::jsonb ->> 0)::int 
+    , trik2_id = (:trik::jsonb ->> 1)::int 
+    , trik3_id = (:trik::jsonb ->> 2)::int 
+    , trik4_id = (:trik::jsonb ->> 3)::int 
+    , trik5_id = (:trik::jsonb ->> 4)::int 
+    , trik6_id = (:trik::jsonb ->> 5)::int 
+    from licznik_trikow
+    where i < 7
+    and id = $id::bigint
+)
+select 'redirect' as component
+    , case when (select i from licznik_trikow)> 6 
+        then '/Admin/modyfikuj_usun_stworzenie_error.sql?gra='||$gra||'&name='||$name||'&gatunek='||$gatunek||''  
+      when (select i from licznik_trikow) < 7
+        then 
+       '/Admin/modyfikuj_usun_stworzenie.sql?gra='||$gra||'' end as link
+      ;
+
+
