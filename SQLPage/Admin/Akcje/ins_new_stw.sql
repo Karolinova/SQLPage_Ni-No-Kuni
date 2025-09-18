e4bd16946a4dbdfe1a2aae9a497df17ff38c09fc
Karolinova <karolina.nowinska13@gmail.com>
2025-09-18 21:17:34 +0200

    correct insert

--- SQLPage/Admin/Akcje/ins_new_stw.sql
+++ SQLPage/Admin/Akcje/ins_new_stw.sql
@@ -2,7 +2,7 @@ with licznik_trikow as (
     select jsonb_array_length(:trik::jsonb) as i
 )
 , wstaw_dane as (
-    insert into stworzenia (nazwa, trik1_id, trik2_id, trik3_id, gatunek_id, trik4_id, trik5_id, trik6_id)
+    insert into stworzenia (nazwa, trik1_id, trik2_id, trik3_id, gatunek_id, trik4_id, trik5_id, trik6_id, gra_id)
     select :name as nazwa
     , (:trik::jsonb ->> 0)::int as trik1_id
     , (:trik::jsonb ->> 1)::int as trik2_id
@@ -11,6 +11,7 @@ with licznik_trikow as (
     , (:trik::jsonb ->> 3)::int as trik4_id
     , (:trik::jsonb ->> 4)::int as trik5_id
     , (:trik::jsonb ->> 5)::int as trik6_id
+    , $gra::BIGINT as gra_id
     from licznik_trikow
     where i < 7
 )
