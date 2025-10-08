867189abea1da48b2231eeca35f92b7d0f570644
Karolinova <karolina.nowinska13@gmail.com>
2025-10-08 20:48:13 +0200

    add column gra_id

--- Database/Ni_No_Kuni/triki_zaklecia.sql
+++ Database/Ni_No_Kuni/triki_zaklecia.sql
@@ -2,5 +2,7 @@
 create table triki_zaklecia (id bigserial primary key
 , nazwa varchar
 , stworzenie_postac varchar(10)
+, gra_id int
+, constraint FK_gra foreign key(gra_id) references lista_gier (id)
 );
 
